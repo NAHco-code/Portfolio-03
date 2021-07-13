5c3c87f009190615605380282df2fc4f82a03cd6
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-07-12 22:13:02 -0400

    Mobile collapse menu in progress - materialize

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
-$( '[data-curtain-menu-button]' ).click( function () {
-    $( 'body' ).toggleClass( 'curtain-menu-open' );
-} )
+// $( '[data-curtain-menu-button]' ).click( function () {
+//     $( 'body' ).toggleClass( 'curtain-menu-open' );
+// } )
+
+// document.addEventListener( 'DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll( '.sidenav' );
+//     var instances = M.Sidenav.init( elems, options );
+// } );
+
+$( document ).ready( function () {
+    $( '.sidenav' ).sidenav();
+} );
