f8c41a5c5b855016766d884908261d89306297a5
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-12-23 04:06:17 -0500

    refactor: Add additional scrolling tickers on contact page; Add phone image; Rework homepage blurb; fix typos + clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,76 @@
-const toggleButton = document.querySelector('.nav').children[1]
-const nav2 = document.querySelector('.nav').children[2]
-const emailButton = document.querySelector('#email-btn')
 
 // Responsive Mobile Menu
+const toggleButton = document.querySelector('.nav').children[1];
+const nav2 = document.querySelector('.nav').children[2];
+
 toggleButton.addEventListener('click', () => {
 	nav2.classList.toggle('active');
 });
 
+// Email Button on Contact page
+const emailButton1 = document.querySelector('div.contact-btn-wrapper a#email-btn1');
+const emailButton2 = document.querySelector('div.contact-btn-wrapper a#email-btn3');
+const emailButton3 = document.querySelector('div.contact-btn-wrapper a#email-btn5');
+const emailText = `kathrynxwilk@gmail.com`;
+const buttonText = `Email`;
 
-const emailText = `<a class="contact-links" href="mailto:kathrynxwilk@gmail.com">
-kathrynxwilk@gmail.com
-</a>`
-const buttonText = `<a class="contact-links" href="mailto:kathrynxwilk@gmail.com">
-Email
-</a>`
+const replaceBtnText = (emailButton) => {
+	emailButton.innerText = emailText;
+	emailButton.style.fontSize = '0.7rem';
+	emailButton.style.color = 'rgb(181, 243, 255)';
+	emailButton.style.backgroundColor = 'rgb(155, 185, 213)';
+	emailButton.style.fontWeight = '600';
+	emailButton.style.lineHeight = '1rem';
+};
+const replaceEmailText = (emailButton) => {
+	emailButton.innerText = buttonText;
+	emailButton.style.fontSize = '1rem';
+	emailButton.style.color = 'rgb(36, 190, 221)';
+	emailButton.style.backgroundColor = 'rgb(227, 235, 244)';
+};
+const replaceBtnText2 = (emailButton) => {
+	emailButton.innerText = emailText;
+	emailButton.style.fontSize = '0.7rem';
+	emailButton.style.color = 'rgb(36, 190, 221)';
+	emailButton.style.backgroundColor = 'rgb(227, 235, 244)';
+	emailButton.style.fontWeight = '600';
+	emailButton.style.lineHeight = '1rem';
 
-emailButton.addEventListener('click', () => {
+};
+const replaceEmailText2 = (emailButton) => {
+	emailButton.innerText = buttonText;
+	emailButton.style.fontSize = '1rem';
+	emailButton.style.color = 'rgb(181, 243, 255)';
+	emailButton.style.backgroundColor = 'rgb(155, 185, 213)';
+};
 
-	if (emailButton.innerHTML == emailText) {
-		emailButton.innerHTML = buttonText;
-		emailButton.style.fontSize = '1rem';
-		emailButton.style.color = 'rgb(220, 249, 255)';
-		emailButton.style.backgroundColor = 'rgb(227, 235, 244)';
-	}
-	else {
-		emailButton.innerHTML = emailText;
-		emailButton.style.fontSize = '0.75rem';
-		emailButton.style.color = ' rgb(181, 243, 255)';
-		emailButton.style.backgroundColor = 'rgb(155, 185, 213)';
-		emailButton.style.fontWeight = '600';
-		emailButton.style.lineHeight = '1rem';
-	}
-})
+const toggleButtonText = (emailButton) => {
+	emailButton.addEventListener('click', () => {
+		if (emailButton.innerText == emailText) {
+			replaceEmailText(emailButton);
+		}
+		else if (emailButton.innerText == buttonText) {
+			replaceBtnText(emailButton);
+		}
+		else {
+			console.log(emailButton)
+		}
+	})
+}
+const toggleButtonText2 = (emailButton) => {
+	emailButton.addEventListener('click', () => {
+		if (emailButton.innerText == emailText) {
+			replaceEmailText2(emailButton);
+		}
+		else if (emailButton.innerText == buttonText) {
+			replaceBtnText2(emailButton);
+		}
+		else {
+			console.log(emailButton)
+		}
+	})
+}
+
+toggleButtonText(emailButton1);
+toggleButtonText2(emailButton2);
+toggleButtonText(emailButton3);
